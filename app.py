cab8c1d3db87517d14a8e4cb002b5f458bd21b6b
Donald Rauscher <donald.rauscher@gmail.com>
2024-04-04 22:39:12 -0500

    Using LLM sherpa to load PDF document in nice sections

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
-def sort_docs_by_page_number(docs: List[Document]) -> List[Document]:
-    return sorted(docs, key=lambda x: x.metadata['page'])  
+def sort_docs_by_section_number(docs: List[Document]) -> List[Document]:
+    return sorted(docs, key=lambda x: x.metadata['section_number'])  
 
 
 @st.cache_resource(show_spinner=False)
@@ -36,7 +36,7 @@ def get_rag_chain(prompt_str: str, k: int = 5):
         search_kwargs={"k": k}
     )
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0)
 
     # prompt = hub.pull("rlm/rag-prompt")
     prompt = ChatPromptTemplate.from_messages([("human", prompt_str)])
@@ -52,7 +52,7 @@ def get_rag_chain(prompt_str: str, k: int = 5):
     )
 
     rag_chain_with_source = RunnableParallel(
-        {"context": retriever | sort_docs_by_page_number, "question": RunnablePassthrough()}
+        {"context": retriever | sort_docs_by_section_number, "question": RunnablePassthrough()}
     ).assign(answer=rag_chain)
 
     return rag_chain_with_source
@@ -80,7 +80,8 @@ prompt = st.text_area(
     value=(
         "You are a medical billing specialist who is helping healthcare providers answer questions related "
         "to claims billing. You will be provided excerpts from CMS' Medicare claims processing manual, which "
-        "you will use to answer the question. If you don't know the answer, just say that you don't know. "
+        "you will use to answer the question. Only use information from the context provider.  If you cannot "
+        "determine the answer based on the context provided, just say that you don't know. "
         "Answers should be detailed with a maximum length of 8 sentences. \n\n"
         "Question: {question} \n\n"
         "Context: {context} \n\n"
@@ -102,6 +103,10 @@ if st.button("Ask", type="primary", disabled=not question):
         st.info(result["answer"])
 
         # show supporting documents
-        for doc in sort_docs_by_page_number(result["context"]):
-            with st.expander(f"Page {str(doc.metadata['page'])}"):
-                st.write(doc.page_content)
+        for doc in sort_docs_by_section_number(result["context"]):
+            expander_title = "Chapter={}; Section={}".format(
+                str(doc.metadata.get('chapter', 'None')), 
+                str(doc.metadata['section_title'])
+            )
+            with st.expander(expander_title):
+                st.markdown(doc.page_content, unsafe_allow_html=True)
--- load.py
+++ load.py
@@ -1,26 +1,49 @@
+import re, os
+
 import chromadb
 from langchain_openai import OpenAIEmbeddings
+
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders.llmsherpa import LLMSherpaFileLoader
+
+
+NLM_INGESTOR_URL = os.environ.get('NLM_INGESTOR_URL', "http://localhost:5010/api/parseDocument?renderFormat=all")
 
 
 if __name__ == "__main__":
+    # load document
+    loader = LLMSherpaFileLoader(
+        file_path="docs/medicare_claims_processing_manual_ch5.pdf",
+        new_indent_parser=True,
+        apply_ocr=True,
+        strategy="sections",
+        llmsherpa_api_url=NLM_INGESTOR_URL
+    )
+    docs = loader.load()
 
-    # load pdf
-    loader = PyPDFLoader("guidelines.pdf")
-    pages = loader.load_and_split()
+    # add chapter information to metadata
+    last_chapter = None
+    for doc in docs:
+        chapter_match = re.compile(r'^([0-9]+(?:\.[0-9]+)*) -').search(doc.metadata['section_title'])
+        if chapter_match:
+            chapter = chapter_match.group(1)
+            doc.metadata['chapter'] = chapter
+            last_chapter = chapter
+        else:
+            if last_chapter is not None:
+                doc.metadata['chapter'] = last_chapter
 
     # embed and load into chroma
     # each page = one document
     # by setting ids for each document, will prevent loading more than once
     vs = Chroma.from_documents(
         collection_name="guidelines",
-        documents=pages,
-        ids=[str(i) for i in range(len(pages))],
+        documents=docs,
+        ids=[str(i) for i in range(len(docs))],
         embedding=OpenAIEmbeddings(), 
         persist_directory="./chroma_db"
     )
 
     # confirm that we have a document for each page
     chroma_client = chromadb.PersistentClient(path="./chroma_db")
-    assert chroma_client.get_collection(name="guidelines").count() == len(pages)
+    assert chroma_client.get_collection(name="guidelines").count() == len(docs)
